3ecaacdd5d1feee3c610e909ce9fdee2f69815f6
Timi Fasubaa <timifasubaa@gmail.com>
2017-07-29 11:41:54 -0700

    fully implemented counter with some bugs

--- counter.py
+++ counter.py
@@ -3,33 +3,50 @@ class Counter:
 	def __init__(self, num_digits, bases):
 		if num_digits != len(bases):
 			raise Exception
+		if 0 in bases:
+			print "Zero is not acceptable as a base here"
+			raise Exception
 
-		self.value = "0"*num_digits
+		self.value = ["0"]*num_digits
 		self.num_increments = 0
 		self.max_increments = reduce(lambda x, y: x*y, bases) - 1
 		self.bases = bases
 		self.num_digits = num_digits
 
-	def can_increment():
+	def can_increment(self, ):
 		return self.num_increments <= self.max_increments
 
-	def increment():
-		assert can_increment()
+	def increment(self, ):
+		assert self.can_increment()
 
-		for i in range(1, self.num_digits+1):
-			curr = int(value[-i])
-			if curr < self.bases[i]:
+		#increment case
+		i=1
+
+		while i < self.num_digits and self.can_increment():
+			curr = int(self.value[-i])
+			if curr < self.bases[-i]-1:
 				curr += 1
-				self.value = str(curr)
-				return 
+				self.value[-i] = str(curr)
+				print "1value is now" + str(self.value)
+				return
 
-#			IF NOT THEN YOUBASICALLU SKIP THIS AND GO STRAIGHT TO THE PART WHERRE RHYCHOOSE AND PAY
-		 	self.value = str(curr)
+			#Carry case
+			self.value[-i] = "0"
+			print "2value is now" + str(self.value)
+			i+=1
+			pos = i 
+			while self.value[-pos] == self.bases[-pos] -1:
+				pos += 1
+			self.value[-pos] = str(int(self.value[-pos])+1)
+			print "3value is now" + str(self.value)
+			self.num_increments += 1
+			return
 
-		self.num_increments += 1
+	def get_curr_value(self, ):
+		return str(self.value)
 
-	def get_curr_value():
-		return self.value
-
-c = Counter(1, [2,])
-c.get_curr_value()
+c = Counter(2, [1,2,])
+print c.get_curr_value()
+while c.can_increment():
+	c.increment()
+	print c.get_curr_value()
