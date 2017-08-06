535e270549e350b31dc0278753c30607dce65cd2
Timi Fasubaa <timifasubaa@gmail.com>
2017-08-05 21:39:18 -0700

    the intonator now gives only words that exist in the dictionary and spells them as fully marked yoruba words, not things like a_do or o_wd_mi

--- intonator.py
+++ intonator.py
@@ -4,6 +4,7 @@ Cool
 """
 import counter
 import transformations
+import dictionary_generator
 
 word = raw_input("type in the sentence you want to intonate\nword: ")
 
@@ -18,19 +19,13 @@ possibility_counter = counter.Counter(len(word), possibilities_per_slot)
 def get_word_generator(possibilities_per_position):
 	def word_generator(number):
 		position_and_selection = enumerate(number)
-		#for i in position_and_selection:
-		#	print i
 		final_word = map(lambda x: possibilities_per_position[x[0]][int(x[1])], position_and_selection)
 		return final_word
 	return word_generator
 
 generate_word = get_word_generator(possibilities_in_TCHSET)
-
-
 all_possibilities = []
-
 selection_number = possibility_counter.get_curr_value()
-
 all_possibilities.append(generate_word(selection_number))
 
 while possibility_counter.can_increment():
@@ -38,5 +33,10 @@ while possibility_counter.can_increment():
 	selection_number = possibility_counter.get_curr_value()
 	all_possibilities.append(generate_word(selection_number))
 
-
-print len(all_possibilities)
+yoruba_dictionary = dictionary_generator.get_yoruba_dictionary()
+#print all_possibilities
+all_possibilities = map(lambda x : "".join(x), all_possibilities)
+verified_words = filter(lambda x : x in yoruba_dictionary, all_possibilities)
+print "There are {0} verified possibilities".format(len(verified_words))
+for word in verified_words:
+	print word
