a492554720305d3800f9cc92b4954e1c24a7a2b0
Timi Fasubaa <timifasubaa@gmail.com>
2017-08-05 23:12:38 -0700

    minor optimizations on dictionary preprocessing

--- dictionary_generator.py
+++ dictionary_generator.py
@@ -1,37 +1,11 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 
-def lower_case(word):
-	"""this function takes in a marked yoruba word and returns the lower case version"""
-	result = ""
-	marked_capital_letters = {"À":"à", 
-							  "Á":"á", 
-							  "É":"é", 
-							  "È":"è",
-							  "Ẹ":"ẹ", 
-							  "Ì":"ì",
-							  "Í":"í",
-							  "Ó":"ó", 
-							  "Ò":"ò", 
-							  "Ṣ":"ṣ", 
-							  "Ọ":"ọ",
-							  "Ú":"ú",
-							  "Ù":"ù"
-							 }
-	for letter in word:
-		if letter in marked_capital_letters.keys():
-			result += marked_capital_letters[letter]
-		else:
-			result += letter.lower()
-	return result
-
-#load the words in the dictionary into a set 
-
 def get_yoruba_dictionary():
+	#load the words in the dictionary into a set 
 	yoruba_words = set() 
-
-	filename = "cleaned_yoruba_dict.txt"
+	filename = "cleaned_yoruba_dict.txt"#replace with the name of your dictionary of yoruba words.
 	file = open(filename, "r")
 	for line in file:
-  		yoruba_words.add(lower_case(line.strip()))
+  		yoruba_words.add(line.strip())
   	return yoruba_words
