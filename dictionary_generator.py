024e04d3278d3555e8fa964095362e47ee40ab1a
Timi Fasubaa <timifasubaa@gmail.com>
2017-09-25 01:47:57 -0700

    fix crash

--- dictionary_generator.py
+++ dictionary_generator.py
@@ -4,7 +4,7 @@
 def get_yoruba_dictionary():
 	#load the words in the dictionary into a set 
 	yoruba_words = set() 
-	filename = "cleaned_yoruba_dict.txt"#replace with the name of your dictionary of yoruba words.
+	filename = "data/cleaned_yoruba_dict.txt"#replace with the name of your dictionary of yoruba words.
 	file = open(filename, "r")
 	for line in file:
   		yoruba_words.add(line.strip())
