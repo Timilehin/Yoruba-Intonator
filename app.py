e02ef684c484a9f6e96926836501d1ce95e4fc82
Timi Fasubaa <timifasubaa@gmail.com>
2017-10-02 00:47:51 -0700

    log failed translations

--- app.py
+++ app.py
@@ -22,12 +22,29 @@ def form():
 	if form.validate_on_submit():
 		translated_sentence = viterbi.get_most_likely_sentence_markings(form.unmarked_yoruba.data)
 		unable_to_translate = form.unmarked_yoruba.data == translated_sentence
-		response = "Sorry, I was unable to translate:</h1>" if unable_to_translate else "<h1> The translated version is:</h1>"
+		if unable_to_translate:
+			import os
+			filename = 'untranslatable.txt'
+
+			if os.path.exists(filename):
+			    append_write = 'a' # append if already exists
+			else:
+			    append_write = 'w' # make a new file if not
+
+			highscore = open(filename,append_write)
+			highscore.write(translated_sentence + '\n')
+			highscore.close()
+
+		response = "<h1>Sorry, I was unable to translate:</h1>" if unable_to_translate else "<h1> The translated version is:</h1>"
 		#log words you can't mark
 
 		return '{}<br/> {} <a href=''><br/>Translate more </a>'.format(response, translated_sentence)
 
 	return render_template('form.html', form=form)
+
+@app.route('/untranslated', methods=['GET', 'POST'])
+def untranslated():
+	return "testing123"
 
 if __name__ == '__main__':
 	app.run(debug=True)
