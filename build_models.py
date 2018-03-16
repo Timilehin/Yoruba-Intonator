6c2140a3691adf926f3da80127e860d286cafd09
Timi Fasubaa <timifasubaa@gmail.com>
2018-03-15 21:24:16 -0700

    separate bigram from semantic bigram

--- build_models.py
+++ build_models.py
@@ -1,8 +1,10 @@
 #make a function that does everything you need and tells you where the files are and progress updates.
-from semantic_bigram import generate_bigram
+from semantic_bigram import generate_bigram, generate_contextual_bigram
 from bigram_probabilities import generate_bigram_probabilities
 filename = "tokenized_sentences.txt"
 print("Generating bigrams from {}".format(filename))
 generate_bigram(filename)
+print("Generating contextual bigrams from {}".format(filename))
+generate_contextual_bigram(filename)
 print("Generating bigram probabilities")
 generate_bigram_probabilities()
--- semantic_bigram.py
+++ semantic_bigram.py
@@ -2,8 +2,29 @@ import collections
 import utils
 import nltk
 
+def generate_contextual_bigram(filename):
+	file = open(filename, "r")
+	semantic_bigram_freq = collections.Counter()
+	bigram_freq = collections.Counter()
 
-def generate_bigram(filename, contextual_bigram=True):
+	for f_line in file:
+		line = f_line.split()
+
+		all_pairs = utils.get_all_pairs(line)
+		for pair in all_pairs:
+			pair.sort()
+			semantic_bigram_freq[tuple(pair)] += 1
+
+	fh = open("contextual_bigram_data.txt","w")
+
+	for words, freq in semantic_bigram_freq.items():
+		fh.write("{},{},{}\n".format(words[0], words[1], freq))
+
+	fh.close()
+
+
+
+def generate_bigram(filename):
 	# given a tokenized file, generate bigram and contextual bigram data 
 	#filename = "tokenized_sentences.txt"
 	#filename = "newbi.txt"
@@ -19,30 +40,9 @@ def generate_bigram(filename, contextual_bigram=True):
 		for gram in bigrams:
 			bigram_freq[gram] += 1
 
-		#semantic_bigram
-		if contextual_bigram:
-			all_pairs = utils.get_all_pairs(line)
-			for pair in all_pairs:
-				pair.sort()
-				semantic_bigram_freq[tuple(pair)] += 1
-
-	if contextual_bigram:
-		fh = open("contextual_bigram_data.txt","w")
-
-		for words, freq in semantic_bigram_freq.items():
-		  	fh.write("{},{},{}\n".format(words[0], words[1], freq))
-
-		fh.close()
-
-
 	bigram_file = open("bigram_data.txt","w")
 
 	for words, freq in bigram_freq.items():
 	  	bigram_file.write("{},{},{}\n".format(words[0], words[1], freq))
 
 	bigram_file.close()
-
-	#make a utils.py to have these things you reuse.
-
-	#have a list of word pairs containing the two words. THey should be interchangeable. 
-	#Also see if you can implement the gaussian approach to share the weight in the sentence pretty equally. 
