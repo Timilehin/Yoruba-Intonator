a049614ca7b3bd23475fc101d4f221b9eb1d517c
Timi Fasubaa <timifasubaa@gmail.com>
2018-03-15 21:43:18 -0700

    updte before the hit

--- dictionary_generator.py
+++ dictionary_generator.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 
+import unicodedata
+
 def get_yoruba_dictionary():
 	#load the words in the dictionary into a set 
 	yoruba_words = set() 
-	filename = "data/cleaned_yoruba_dict.txt"#replace with the name of your dictionary of yoruba words.
+	filename = "data/dictionary.txt"#replace with the name of your dictionary of yoruba words.
 	file = open(filename, "r")
 	for line in file:
-  		yoruba_words.add(line.strip())
+		#print line
+		item = unicodedata.normalize('NFD', line.strip().decode("utf-8")) 
+  		yoruba_words.add(item)
   	return yoruba_words
+
+"""fh = open("unique_dict.txt", "w")
+
+words = get_yoruba_dictionary()
+for word in words:
+  	fh.write("{}\n".format(word))
+
+fh.close()"""
